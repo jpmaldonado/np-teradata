f05f30274a38cda495d5c8d462a6cad449d87d1c
Pablo Maldonado <pablo@datastart.eu>
2019-05-22 22:39:29 +0200

    day 2

--- scripts/02 Media share analytics.sql
+++ scripts/02 Media share analytics.sql
@@ -1,9 +1,9 @@
 CREATE VOLATILE TABLE media(id INTEGER, url VARCHAR(100),  shares INTEGER) PRIMARY INDEX(id)
 ON COMMIT PRESERVE ROWS;
 
-INSERT INTO media(id, url,shares) VALUES(?, ?, ?); /* Set up correctly the import options in Tools | Options! */ 
+INSERT INTO media(d, url,shares) VALUES(?, ?, ?); /* Set up correctly the import options in Tools | Options! */ 
 
-SELECT TOP 10 *  FROM media; /* Don't forget to "turn down" the import mode */
+SELECT TOP 10 *  FROM media; /* Don't forget to "turn down" the iport mode */
 
 SELECT TOP 10 Substr(url, 21,10) AS article_date FROM media /* extract date */
 
